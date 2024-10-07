db59c54f1abd45d5c0bcf55fd15f8b8b5200a064
Silvigauto <silvigauto02@gmail.com>
2024-10-06 22:01:18 -0400

    punto c-e con permisos de ejecucion

---
+++ RTA_SCRIPT_Examen_20241006/Punto_E.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd **~/repogit/UTNFRA_SO_1P2C_2024_GAUTO/RTA_ARCHIVOS_Examen_20241006**
+
+sudo cat /proc/meminfo | grep -i memtotal | awk -F ':' '{print$2}' > Filtro_Basico.txt
+
+sudo dmidecode -t chassis | grep -i manufacturer >> Filtro_Basico.txt
